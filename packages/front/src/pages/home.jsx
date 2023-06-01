1396045ae8b190afad1f24c8be59b7bd16b2595b
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-01 20:08:02 +0200

    Actualización pasado a vite + react

--- packages/front/src/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-import { BannerHome } from '../components/banner/banner';
-import { NavigationMenu } from '../components/nav/nav';
-
-export function Home() {
-    return(
-        <NavigationMenu></NavigationMenu>
-        //<BannerHome></BannerHome>
-    )
-}
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+//import { BannerHome } from '../components/banner/banner';
+import { NavigationMenu } from '../components/nav/nav';
+
+export function Home() {
+    return(
+        <NavigationMenu></NavigationMenu>
+        //<BannerHome></BannerHome>
+        //<h1>Hola</h1>
+        //<BannerHome></BannerHome>
+    )
+}
