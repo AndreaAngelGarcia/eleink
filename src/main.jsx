e39a3176532824435d65c4eaa8368aa7d57eade1
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-05 19:56:08 +0200

    Actualización main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Home } from './pages/home.jsx'
 //import { NavigationMenu } from '.components/nav/nav.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Home></Home>
   </React.StrictMode>,
 )
