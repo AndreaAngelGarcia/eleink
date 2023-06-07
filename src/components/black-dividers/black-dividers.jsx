fdbd0e55ca3f05b3e88abb9aea5a878fe2f9e1ca
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-07 13:25:05 +0200

    Creación componente separadores

---
+++ src/components/black-dividers/black-dividers.jsx
@@ -0,0 +1,8 @@
+export function BlackDividers() {
+    return (
+        <div className="div">
+            <img src="./src/assets/images/foto.png" alt="Separador" />
+            <img src="./src/assets/images/foto4.png" alt="Separador" />
+        </div>
+    )
+}
