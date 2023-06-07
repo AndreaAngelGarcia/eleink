44cdb4ad05d951d0024e1258210d0f7ad8f1a0da
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-07 20:34:42 +0200

    Actualización

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -2,12 +2,14 @@ import { NavigationMenu } from '../components/nav/nav';
 import { Footer } from '../components/footer/footer';
 import { Tittle } from '../components/tittle/tittle';
 import { ButtonBackToTop } from '../components/ButtonBackToTop/button-back-to-top';
+import { BlackDividers } from '../components/black-dividers/black-dividers';
 
 export function ShopPage() {
     return (
         <div className="m-0 p-0">
             <ButtonBackToTop />
             <NavigationMenu />
+            <BlackDividers/>
             <Tittle title="Tienda" />
             <Footer />
         </div>
