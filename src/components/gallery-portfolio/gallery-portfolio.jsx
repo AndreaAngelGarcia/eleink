bf46ce5eeee194c230dd2383bfd2ee256b5ae341
ManuelGuerraRuiz <andrea.angel@adaits.es>
2023-06-10 13:30:27 +0200

    Nuevo componente galería en página portfolio

---
+++ src/components/gallery-portfolio/gallery-portfolio.jsx
@@ -0,0 +1,80 @@
+export function GalleryPortfolio({ activeContent }) {
+    return (
+        <div>
+            {/* GALERIA ELEINK */}
+            <div id="content-eleink"
+                className={`content container mx-auto mt-5 p-5 sm:p-8 ${activeContent === 'content-eleink' ? 'active' : ''}`}
+                data-aos="fade-up"
+                data-aos-duration="5000">
+                <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+                    <img src="./src/assets/images/tatuajes/tatuaje8.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje18.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje17.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje7.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje5.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje2.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje60.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje10.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje42.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje59.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje3.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje12.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje13.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje14.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje61.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje16.jpeg" data-aos="fade-up" alt="" />
+                </div>
+            </div>
+
+            {/* GALERIA ELETINYTATS */}
+            <div id="content-eletinytats"
+                className={`content container mx-auto mt-8 p-5 sm:p-8 ${activeContent === 'content-eletinytats' ? 'active' : ''}`}
+                data-aos="fade-up"
+                data-aos-duration="5000">
+                <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+                    <img src="./src/assets/images/tatuajes/tatuaje22.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje25.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje24.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje26.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje29.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje27.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje33.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje34.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje28.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje35.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje31.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje36.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje37.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje38.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje39.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje40.jpeg" data-aos="fade-up" alt="" />
+                </div>
+            </div>
+
+            {/* GALERIA PIERCING */}
+            <div id="content-piercing"
+                className={`content container mx-auto mt-8 p-5 sm:p-8 ${activeContent === 'content-piercing' ? 'active' : ''}`}
+                data-aos="fade-up"
+                data-aos-duration="5000">
+                <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+                    <img src="./src/assets/images/tatuajes/tatuaje43.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje44.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje45.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje46.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje47.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje46.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje58.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje50.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje51.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje52.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje53.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje54.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje55.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje56.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje57.jpeg" data-aos="fade-up" alt="" />
+                    <img src="./src/assets/images/tatuajes/tatuaje49_4.jpeg" data-aos="fade-up" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
