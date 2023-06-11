c63d8f6d8967499d8086395e9d2de3baaa5ea957
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-12 00:05:07 +0200

    Update signin.jsx

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -2,12 +2,14 @@ import { Footer } from '../components/footer/footer';
 import { Register2 } from '../components/register/register2';
 import { ButtonBackToTop } from '../components/button-back-to-top/button-back-to-top';
 import { NavigationMenu } from '../components/nav/nav';
+import { BlackDividers } from '../components/black-dividers/black-dividers';
 
 export function SigninPage() {
     return (
         <div className="m-0 p-0">
             <ButtonBackToTop />
             <NavigationMenu />
+            <BlackDividers />
             <Register2 />
             <Footer />
         </div>
