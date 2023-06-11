beff69d66dd2ad8d8f3c1075524f32cff2e108cc
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-11 21:58:25 +0200

    Update about.jsx

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -19,12 +19,12 @@ export function AboutHome() {
                 <div className="flex items-center justify-center -mx-4 lg:pl-8">
                     <div className="flex flex-col items-end px-3">
                         <img className="object-cover mb-6 rounded shadow-lg h-32 sm:h-52 xl:h-60 w-32 sm:w-52 xl:w-60"
-                            src="./src/assets/images/elena.jpg" alt="" />
-                        <img className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40" src="./src/assets/images/elena3.jpg"
+                            src="./public/assets/images/elena.jpg" alt="" />
+                        <img className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40" src="./public/assets/images/elena3.jpg"
                             alt="" />
                     </div>
                     <div className="px-3">
-                        <img className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80" src="./src/assets/images/elena4.jpg"
+                        <img className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80" src="./public/assets/images/elena4.jpg"
                             alt="" />
                     </div>
                 </div>
