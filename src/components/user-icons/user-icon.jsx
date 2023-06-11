fee15f616c0e8abf7e57206c1891d6cc0c3e5b5e
ManuelGuerraRuiz <andrea.angel@adaits.es>
2023-06-11 19:40:09 +0200

    Actualización

--- src/components/user-icons/user-icon.jsx
+++
@@ -1,9 +0,0 @@
-import { AiOutlineUser } from 'react-icons/ai';
-
-export function UserIcon() {
-  return (
-    <div>
-      <AiOutlineUser />
-    </div>
-  );
-}
--- src/components/user-icons/user-icon2.jsx
+++
@@ -1,9 +0,0 @@
-import { FaUserCheck } from 'react-icons/fa';
-
-export function UserIcon2() {
-  return (
-    <div>
-      <FaUserCheck />
-    </div>
-  );
-}
