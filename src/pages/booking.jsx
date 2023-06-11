dbbf1b5f32072d17e969263610d6a68db6c88008
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-12 00:30:00 +0200

    Update booking.jsx

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -2,11 +2,13 @@ import { Footer } from '../components/footer/footer';
 import { BlackDividers } from '../components/black-dividers/black-dividers';
 import { Booking } from '../components/booking/booking';
 import { ButtonBackToTop } from '../components/button-back-to-top/button-back-to-top';
+import { NavigationMenu } from '../components/nav/nav';
 
 export function BookingPage() {
     return (
         <div className="m-0 p-0">
             <ButtonBackToTop />
+            <NavigationMenu />
             <BlackDividers />
             <Booking />
             <Footer />
