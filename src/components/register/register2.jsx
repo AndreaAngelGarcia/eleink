544b7429dd8e0ad68c12530a4a6eae0321367152
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-12 16:41:40 +0200

    Update register2.jsx

--- src/components/register/register2.jsx
+++ src/components/register/register2.jsx
@@ -19,7 +19,7 @@ export function Register2() {
     };
 
     const handleSubmit = (x) => {
-        x.preventDefault(); 
+        x.preventDefault();
         fetch('https://eleink-openapi.onrender.com/signin', {
             method: 'POST',
             headers: {
@@ -37,112 +37,110 @@ export function Register2() {
     };
 
     return (
-        <section>
-            <div className=" min-h-screen">
-                <div className="flex flex-col items-center justify-center bg-white rounded-tl-lg rounded-bl-lg m-0">
-                    <div className="w-full">
-                        <form>
-                            {/*NOMBRE*/}
-                            <div className="flex justify-center">
-                                <div className="relative w-72 mt-5 ">
-                                    <input type="text" name="name" id="name" placeholder="Correo electrónico" onChange={handleChange}
-                                        className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                                        autoComplete="off" required />
-                                    <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
+        <section className="mb-8">
+            <div className="flex flex-col items-center justify-center bg-white rounded-tl-lg rounded-bl-lg m-0">
+                <div className="w-full">
+                    <form>
+                        {/*NOMBRE*/}
+                        <div className="flex justify-center">
+                            <div className="relative w-72 mt-5 ">
+                                <input type="text" name="name" id="name" placeholder="Correo electrónico" onChange={handleChange}
+                                    className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                                    autoComplete="off" required />
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
                                         ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 
                                         peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">
-                                        Nombre
-                                    </label>
-                                </div>
+                                    Nombre
+                                </label>
                             </div>
+                        </div>
 
-                            {/*EMAIL*/}
-                            <div className="flex justify-center">
-                                <div className="relative w-72 mt-5 ">
-                                    <input type="email" name="email" id="email" placeholder="Correo electrónico" onChange={handleChange}
-                                        className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                                        autoComplete="off" required />
-                                    <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
+                        {/*EMAIL*/}
+                        <div className="flex justify-center">
+                            <div className="relative w-72 mt-5 ">
+                                <input type="email" name="email" id="email" placeholder="Correo electrónico" onChange={handleChange}
+                                    className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                                    autoComplete="off" required />
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
                                         ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 
                                         peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">
-                                        Correo electrónico
-                                    </label>
-                                </div>
+                                    Correo electrónico
+                                </label>
                             </div>
+                        </div>
 
-                            {/*FECHA NACIMIENTO*/}
-                            <div className="flex justify-center">
-                                <div className="relative w-72 mt-5 ">
-                                    <input type="text" name="birthday" id="birthday" placeholder="Correo electrónico" onChange={handleChange}
-                                        className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                                        autoComplete="off" required />
-                                    <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
+                        {/*FECHA NACIMIENTO*/}
+                        <div className="flex justify-center">
+                            <div className="relative w-72 mt-5 ">
+                                <input type="text" name="birthday" id="birthday" placeholder="Correo electrónico" onChange={handleChange}
+                                    className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                                    autoComplete="off" required />
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
                                         ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 
                                         peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">
-                                        Fecha nacimiento
-                                    </label>
-                                </div>
+                                    Fecha nacimiento
+                                </label>
                             </div>
+                        </div>
 
-                            {/*TELÉFONO*/}
-                            <div className="flex justify-center">
-                                <div className="relative w-72 mt-5 ">
-                                    <input type="phone" name="phone" id="phone" placeholder="Correo electrónico" onChange={handleChange}
-                                        className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                                        autoComplete="off" required />
-                                    <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
+                        {/*TELÉFONO*/}
+                        <div className="flex justify-center">
+                            <div className="relative w-72 mt-5 ">
+                                <input type="phone" name="phone" id="phone" placeholder="Correo electrónico" onChange={handleChange}
+                                    className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                                    autoComplete="off" required />
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
                                         ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 
                                         peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">
-                                        Teléfono
-                                    </label>
-                                </div>
+                                    Teléfono
+                                </label>
                             </div>
+                        </div>
 
-                            {/*USUARIO INSTAGRAM*/}
-                            <div className="flex justify-center">
-                                <div className="relative w-72 mt-5 ">
-                                    <input type="text" name="userInstagram" id="userInstagram" placeholder="Correo electrónico" onChange={handleChange}
-                                        className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                                        autoComplete="off" required />
-                                    <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
+                        {/*USUARIO INSTAGRAM*/}
+                        <div className="flex justify-center">
+                            <div className="relative w-72 mt-5 ">
+                                <input type="text" name="userInstagram" id="userInstagram" placeholder="Correo electrónico" onChange={handleChange}
+                                    className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                                    autoComplete="off" required />
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
                                         ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 
                                         peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">
-                                        Usuario instagram
-                                    </label>
-                                </div>
+                                    Usuario instagram
+                                </label>
                             </div>
+                        </div>
 
-                            {/*CONTRASEÑA*/}
-                            <div className="flex justify-center  mb-6">
-                                <div className="relative w-72 mt-5 ">
-                                    <input type="password" name="password" id="password" placeholder="Correo electrónico" onChange={handleChange}
-                                        className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                                        autoComplete="off" required />
-                                    <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
+                        {/*CONTRASEÑA*/}
+                        <div className="flex justify-center  mb-6">
+                            <div className="relative w-72 mt-5 ">
+                                <input type="password" name="password" id="password" placeholder="Correo electrónico" onChange={handleChange}
+                                    className="peer w-full max-w-md rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                                    autoComplete="off" required />
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 
                                         ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 
                                         peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">
-                                        Contraseña
-                                    </label>
-                                </div>
+                                    Contraseña
+                                </label>
                             </div>
+                        </div>
 
-                            <div className="w-full max-w-md mx-auto mb-4">
-                                <Link to="/login"><button type="submit" onClick={handleSubmit} className="w-full bg-gray-200 py-2 px-4 rounded-lg text-gray-900 text-bold  hover:bg-gray-300 transition-colors">
-                                    Regístrate
-                                </button>
-                                </Link>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div>
-                        <span className="text-gray-500">
-                            ¿Ya tienes cuenta?{" "}
-                            <Link to="/login" className="text-gray-900 hover:underline transition-all">
-                                Inicia sesión
+                        <div className="w-full max-w-md mx-auto mb-4">
+                            <Link to="/login"><button type="submit" onClick={handleSubmit} className="w-full bg-gray-200 py-2 px-4 rounded-lg text-gray-900 text-bold  hover:bg-gray-300 transition-colors">
+                                Regístrate
+                            </button>
                             </Link>
-                        </span>
-                    </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div>
+                    <span className="text-gray-500">
+                        ¿Ya tienes cuenta?{" "}
+                        <Link to="/login" className="text-gray-900 hover:underline transition-all">
+                            Inicia sesión
+                        </Link>
+                    </span>
                 </div>
             </div>
         </section>
