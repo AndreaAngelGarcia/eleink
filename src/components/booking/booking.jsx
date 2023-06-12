ac8e21f19767eeae513b95f128c0b9a81cff6814
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-12 13:29:23 +0200

    Update booking.jsx

--- src/components/booking/booking.jsx
+++ src/components/booking/booking.jsx
@@ -14,12 +14,12 @@ export function Booking() {
             <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-white py-6 sm:py-12 lg:ml-32">
                 <div className="relative border mx-auto w-full max-w-md bg-white px-8 pt-10 pb-8 shadow-xl sm:rounded-xl sm:px-10">
                     <div className="w-full">
-                        <form action="">
+                        <form action="/mailform" method="POST">
                             <div className="relative mt-5">
-                                <input type="name" name="nombre" id="email" placeholder="Nombre y apelidos"
+                                <input type="name" name="name" id="name" placeholder="Nombre y apelidos"
                                     className="peer w-full rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
                                     autoComplete="off" required />
-                                <label htmlFor="email"
+                                <label htmlFor="name"
                                     className="pointer-events-none absolute top-0 left-0 ml-3 origin-left -translate-y-1/2 transform bg-white px-1 text-sm text-gray-500 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:ml-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-0 peer-focus:ml-3 peer-focus:text-sm peer-focus:text-gray-800">Nombre
                                     y apellidos</label>
                             </div>
@@ -32,7 +32,7 @@ export function Booking() {
                                 </label>
                             </div>
                             <div className="relative mt-5">
-                                <input type="phone" name="Teléfono" id="phone" placeholder="Teléfono"
+                                <input type="phone" name="telephone" id="phone" placeholder="Teléfono"
                                     className="peer w-full rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
                                     autoComplete="off" required />
                                 <label htmlFor="email"
@@ -40,7 +40,7 @@ export function Booking() {
                                 </label>
                             </div>
                             <div className="relative mt-5">
-                                <input type="text" name="text" id="instagram" placeholder="Instagram"
+                                <input type="text" name="instagram" id="instagram" placeholder="Instagram"
                                     className="peer w-full rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
                                     autoComplete="off" />
                                 <label htmlFor="email"
@@ -59,11 +59,11 @@ export function Booking() {
 
                             <div className="relative mt-5">
                                 <label htmlFor="imagen">Subir imagen:</label>
-                                <input type="file" id="imagen" name="imagen" /><br /><br />
+                                <input type="file" id="image" name="image" /><br /><br />
                             </div>
 
                             <div className="relative mt-5">
-                                <textarea type="descripcion" name="description" id="descripcion" placeholder="Descripcion"
+                                <textarea type="descripcion" name="description" id="description" placeholder="Descripcion"
                                     className="peer peer w-full rounded-md border border-gray-300 px-3 py-3 shadow shadow-gray-100 placeholder:text-transparent focus:border-gray-500 focus:outline-none"></textarea>
 
                                 <label htmlFor="descripcion"
