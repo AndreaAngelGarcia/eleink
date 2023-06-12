b1c1cf173364aec57e0fd834beb028bedc0515eb
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-12 14:08:46 +0200

    Update booking.jsx

--- src/components/booking/booking.jsx
+++ src/components/booking/booking.jsx
@@ -21,7 +21,7 @@ export function Booking() {
         x.preventDefault();
 
         try {
-            const response = await fetch('https://localhost:3000/mailform', {
+            const response = await fetch('http://localhost:3000/mailform', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
