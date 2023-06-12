6ad60d60b0b20a96a075e47c767b214cb9249ef4
Andrea Ángel García <andrea.angel@adaits.es>
2023-06-12 11:59:55 +0200

    Actualización página portfolio

--- src/components/gallery-portfolio/gallery-portfolio.jsx
+++ src/components/gallery-portfolio/gallery-portfolio.jsx
@@ -1,111 +1,133 @@
-import { useState, useEffect } from 'react';
+import  { useState } from 'react';
+import { useTransition, animated } from 'react-spring';
 
-export function GalleryPortfolio({ activeContent }) {
-  const [isVisible, setIsVisible] = useState(false);
+export function GalleryPortfolio() {
+  const [activeContent, setActiveContent] = useState('content-eleink');
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const contentElement = document.getElementById(activeContent);
-      if (contentElement) {
-        const rect = contentElement.getBoundingClientRect();
-        setIsVisible(rect.top < window.innerHeight);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [activeContent]);
-
-  const galleryStyle = {
-    opacity: isVisible ? 1 : 0,
-    transition: 'opacity 1s ease-in-out',
+  const showContent = (contentId) => {
+    setActiveContent(contentId);
   };
+
+  const transitions = useTransition(activeContent, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 500 },
+  });
 
   return (
     <div>
-      {/* GALERIA ELEINK */}
-      <div
-        id="content-eleink"
-        className={`content container mx-auto mt-5 p-5 sm:p-8 ${activeContent === 'content-eleink' ? 'fadeIn' : ''}`}
-        data-aos="fade-up"
-        style={activeContent === 'content-eleink' ? galleryStyle : {}}
-      >
-        <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-          <img src="./assets/images/tatuajes/tatuaje8.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje18.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje17.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje7.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje5.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje2.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje60.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje10.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje42.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje59.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje3.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje12.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje13.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje14.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje61.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje16.jpeg" data-aos="fade-up" alt="" />
-        </div>
+      {/* BOTONES */}
+      <div className="flex justify-center mt-6 p-6 space-x-4 md:space-x-8 lg:space-x-20 lg:mt-4">
+        <button
+          id="button1"
+          className="w-40 h-40 shadow-xl rounded-xl border border-purple-700 inline-flex items-center"
+          onClick={() => showContent('content-eleink')}
+        >
+          <img
+            src="./assets/images/LOGO.PNG"
+            className="w-24 hover:w-28 lg:w-28 mx-auto lg:hover:w-32"
+            alt=""
+          />
+        </button>
+
+        <button
+          id="button2"
+          className="w-40 h-40 shadow-xl rounded-xl border border-purple-700 inline-flex items-center"
+          onClick={() => showContent('content-eletinytats')}
+        >
+          <img
+            src="./assets/images/LOGO2.png"
+            className="w-24 hover:w-28 lg:w-28 mx-auto lg:hover:w-32"
+            alt=""
+          />
+        </button>
+
+        <button
+          id="button3"
+          className="w-40 h-40 shadow-xl rounded-xl border border-purple-700 inline-flex items-center"
+          onClick={() => showContent('content-piercing')}
+        >
+          <img
+            src="./assets/images/piercing.jpg"
+            className="w-24 hover:w-28 lg:w-28 mx-auto lg:hover:w-32"
+            alt=""
+          />
+        </button>
       </div>
 
-      {/* GALERIA ELETINYTATS */}
-      <div
-        id="content-eletinytats"
-        className={`content container mx-auto mt-8 p-5 sm:p-8 ${activeContent === 'content-eletinytats' ? 'fadeIn' : ''}`}
-        data-aos="fade-up"
-        style={activeContent === 'content-eletinytats' ? galleryStyle : {}}
-      >
-        <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-          <img src="./assets/images/tatuajes/tatuaje22.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje25.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje24.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje26.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje29.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje27.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje33.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje34.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje28.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje35.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje31.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje36.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje37.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje38.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje39.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje40.jpeg" data-aos="fade-up" alt="" />
+      {/* GALERIA */}
+      {transitions((styles, item) => (
+        <animated.div
+          className={`content container mx-auto mt-5 p-5 sm:p-8 ${
+            activeContent === item ? 'active' : ''
+          }`}
+          style={styles}
+        >
+          {activeContent === 'content-eleink' && (
+            <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+            <img src="./assets/images/tatuajes/tatuaje8.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje18.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje17.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje7.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje5.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje2.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje60.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje10.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje42.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje59.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje3.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje12.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje13.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje14.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje61.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje16.jpeg" data-aos="fade-up" alt="" />
         </div>
-      </div>
+          )}
 
-      {/* GALERIA PIERCING */}
-      <div id="content-piercing"
-        className={`content container mx-auto mt-8 p-5 sm:p-8 ${activeContent === 'content-piercing' ? 'fadeIn' : ''}`}
-        data-aos="fade-up"
-        style={activeContent === 'content-piercing' ? galleryStyle : {}}>
-        <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-          <img src="./assets/images/tatuajes/tatuaje43.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje44.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje45.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje46.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje47.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje46.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje58.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje50.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje51.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje52.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje53.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje54.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje55.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje56.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje57.jpeg" data-aos="fade-up" alt="" />
-          <img src="./assets/images/tatuajes/tatuaje49_4.jpeg" data-aos="fade-up" alt="" />
+          {activeContent === 'content-eletinytats' && (
+            <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+            <img src="./assets/images/tatuajes/tatuaje22.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje25.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje24.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje26.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje29.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje27.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje33.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje34.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje28.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje35.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje31.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje36.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje37.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje38.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje39.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje40.jpeg" data-aos="fade-up" alt="" />
         </div>
-      </div>
+          )}
+
+          {activeContent === 'content-piercing' && (
+            <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
+            <img src="./assets/images/tatuajes/tatuaje43.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje44.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje45.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje46.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje47.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje46.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje58.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje50.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje51.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje52.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje53.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje54.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje55.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje56.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje57.jpeg" data-aos="fade-up" alt="" />
+            <img src="./assets/images/tatuajes/tatuaje49_4.jpeg" data-aos="fade-up" alt="" />
+        </div>
+          )}
+        </animated.div>
+      ))}
     </div>
   );
 }
-
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -2,7 +2,6 @@ import { Footer } from '../components/footer/footer';
 import { Tittle } from '../components/tittle/tittle';
 import { ButtonBackToTop } from '../components/button-back-to-top/button-back-to-top';
 import { BlackDividers } from '../components/black-dividers/black-dividers';
-import { ButtonsPortfolio } from '../components/buttons-portfolio/buttons-portfolio';
 import { GalleryPortfolio } from '../components/gallery-portfolio/gallery-portfolio';
 import { NavigationMenu } from '../components/nav/nav';
 
@@ -13,7 +12,6 @@ export function PortfolioPage() {
             <NavigationMenu />
             <BlackDividers />
             <Tittle title="Porfolio" />
-            <ButtonsPortfolio />
             <GalleryPortfolio />
             <Footer />
         </div>
